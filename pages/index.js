7ac9c575445295b613b82af1cb269b793fd3128a
EduClerici <educlerici@gmail.com>
2018-04-27 14:31:41 -0300

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Layout from '../components/MyLayout.js'
 import Link from 'next/link'
-import loadDB from '../lib/load-db'
+import 'fetch-everywhere';
 
 const PostLink = (props) => (
   <li>
@@ -26,20 +26,17 @@ const Index = ({ stories }) => (
 )
 
 Index.getInitialProps = async () => {
-  const db = await loadDB()
-
-  const ids = await db.child('topstories').once('value')
-  let stories = await Promise.all(
-    ids.val().slice(0, 10).map(id => db
-      .child('item')
-      .child(id)
-      .once('value')
-    )
-  )
-
-  stories = stories.map(s => s.val())
-
-  return { stories }
+    const parseString = require('react-native-xml2js').parseString;
+  
+      fetch('http://www.elesquiu.com/rss/feed.html?r=10')
+          .then(response => response.text())
+          .then((response) => {
+              parseString(response, function (err, result) {
+                  console.log(response)
+              });
+          }).catch((err) => {
+              console.log('fetch', err)
+          })
 }
 
 export default Index
